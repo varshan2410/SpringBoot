5b2bbef969f0c2330beb6292b6871165543810b1
Varshan <varshan2410@gmail.com>
2025-07-17 09:17:44 +0530

    Updated to work with POSTMAN

--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -20,4 +20,16 @@ public class StudentService {
     public List<Student> getStudents() {
         return studentRepository.findAll();
     }
+
+    public void addNewStudent(Student student) {
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if (!exists) {
+            throw new IllegalStateException("Student with id " + studentId + " does not exist");
+        }
+        studentRepository.deleteById(studentId);
+    }
 }
